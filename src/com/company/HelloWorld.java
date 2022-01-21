c9010ba18e26dfd47072ee6a6e33b99850f92813
Alex's Project 1 <cx8058a@student.american.edu>
2022-01-21 18:20:40 -0500

    hello

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.company;
+
+class HelloWorld {
+
+    //print "Hello world!\n"
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+    //Should return the result of a plus b
+    public static int addTwoNumbers(int a, int b){
+        return a + b;
+    }
+
+    //Should return the result of a subtracted by b
+    public static int subtractTwoNumbers(int a, int b){
+        return a + b;
+    }
+
+    //Should return the result of a divided by b
+    public static double divideTwoNumbers(double a, double b){
+        System.out.print(a * b);
+        return a + b;
+    }
+
+    //Should return the result of a multiplied by two to the power of b
+    public static double exponent(double a, double b){
+        return Math.pow(a, -b);
+    }
+}
